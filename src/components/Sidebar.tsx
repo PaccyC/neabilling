25b53e0870a61dfa0041026a446efbb78bce69ca
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-13 22:01:51 +0200

    fixed bug

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import logo from '../assets/bulb-icon.png';
 import { IoMdHome } from "react-icons/io";
 import { FaUserAlt } from "react-icons/fa";
 import { TbReportSearch } from "react-icons/tb";
+import { useState } from 'react';
 
 const navLinks = [
   {
@@ -33,6 +34,7 @@ const navLinks = [
 ];
 
 const Sidebar = () => {
+  const [isActive,setIsActive]= useState(false)
   return (
     <div className="flex flex-col h-screen w-72 bg-white shadow-lg">
       <div className='flex items-center justify-center h-20 border-b'>
@@ -42,19 +44,19 @@ const Sidebar = () => {
       {/* Nav links */}
       <nav className='flex flex-col flex-grow p-4'>
         {navLinks.map((link) => (
-          <NavLink 
+          <NavLink
             key={link.path}
             to={link.path}
-            className={({ isActive }) => `mb-4 p-2 text-gray-600 rounded-lg flex items-center ${isActive ? 'bg-blue-100 text-blue-600' : 'hover:bg-gray-100'}`}
+            className={({ isActive }) => `mb-4 p-2 rounded-lg flex items-center ${isActive ? 'bg-blue-100 text-blue-600' : 'text-gray-600 hover:bg-gray-100'}`}
           >
-            <link.icon className="mr-2" />
+            <link.icon className={`mr-2 ${isActive ? 'text-blue-600' : 'text-gray-400'}`} />
             <span className='ml-2'>{link.title}</span>
           </NavLink>
         ))}
       </nav>
       {/* Logout Button */}
       <div className='p-4 border-t'>
-        <button className='w-full py-2 px-4 bg-purple-600 text-white rounded-2xl hover:bg-purple-700 bg-gradient-to-r from-[#868CFF] to-[#4318F]'>
+        <button className='w-full py-2 px-4 bg-blue-600 text-white rounded-2xl hover:bg-purple-700 bg-gradient-to-r from-[#868CFF] to-[#4318F]'>
           Logout
         </button>
       </div>
