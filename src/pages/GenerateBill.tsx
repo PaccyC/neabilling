d34616c0733dbeee5c94a47f4acfda1df68cac0c
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-14 17:00:11 +0200

    completed generate bill page

--- src/pages/GenerateBill.tsx
+++ src/pages/GenerateBill.tsx
@@ -1,3 +1,4 @@
+import { FaCalendarAlt } from "react-icons/fa"
 import SearchBar from "../components/SearchBar"
 
 const GenerateBill = () => {
@@ -12,7 +13,60 @@ const GenerateBill = () => {
 
       <SearchBar/>
      </div>
-     <div className=" h-8 bg-white">
+     <div className=" h-[70vh] bg-white shadow-md  p-4 rounded-xl">
+      <h1 className=" text-[#a2adeb] text-2xl font-semibold mb-6">Calculate Bill</h1>
+      <form className=" space-y-6">
+
+      <div >
+      <label className=" block first-line:text-[#2B3674]">UID</label>
+      <input 
+            type="text"
+            className=" mt-1 p-2 w-full border border-blue-500 rounded"
+      />
+      </div>
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 ">
+        <div className=" relative">
+          <label className=" block text-[#2B3674]">Date</label>
+          <div className=" relative mt-1">
+            <input 
+               type="date"
+               className=" p-2 w-full border border-blue-500 rounded" />
+               <FaCalendarAlt className=" absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"/>
+          </div>
+        </div>
+        <div className=" relative">
+          <label className=" block text-[#2B3674]">Due Date</label>
+          <div className=" relative mt-1">
+            <input 
+               type="date"
+               className=" p-2 w-full border border-blue-500 rounded" />
+               <FaCalendarAlt className=" absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"/>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <label className=" block text-[#2B3674]">Units</label>
+        <input 
+            type="number"
+            className="mt-1 p-2 w-full border border-blue-500 rounded" />
+      </div>
+      <div>
+        <label className="block text-[#2B3674]">Bill Due</label>
+        <input 
+          type="text"
+          className=" mt-1 p-2 w-full border border-blue-500 rounded" />
+      </div>
+      <div className=" flex justify-end">
+      <button
+        type="submit"
+        className="px-4 py-2 bg-[#4318FF] text-white rounded hover:bg-blue-600 right-3">
+          Calculate
+        </button>
+        
+       </div>
+
+      </form>
 
      </div>
 
